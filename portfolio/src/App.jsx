fbd16f8f9c3072771c48e40d51a617dbad8b0ac9
Kiran9223 <88050617+Kiran9223@users.noreply.github.com>
2024-06-22 15:37:18 +0530

    Completed nav bar

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,13 +1,20 @@
-import './App.css'
+import Hero from './components/Hero'
 import NavBar from './components/NavBar'
 
 function App() {
 
   return (
-    <>
-    <NavBar/>
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+    <div className="flixed top-0 -z-10 h-full w-full">
+    <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+    </div>
+
+    <div className="container mx-auto px-2">
+      <NavBar/>
+      <Hero/>
+    </div>
       
-    </>
+    </div>
   )
 }
 
--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -1,12 +1,20 @@
-export default function NavBar() {
-    return(
-        <div>
-            <ul>
-                <li>Home</li>
-                <li>Experience</li>
-                <li>Projects</li>
-                <li>Contact</li>
-            </ul>
+import logo from "../assets/logo.png";
+import {FaLinkedin} from "react-icons/fa";
+import {FaGithub} from "react-icons/fa";
+
+const NavBar = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+        <div className="flex flex-shrink-0 items-center">
+            <img className="mx-2 object-contain h-8 w-8" src={logo} alt="My logo" />
         </div>
-    )
+        <div className="m-2 flex items-center justify-center gap-4 text-2xl">
+            <FaLinkedin/>
+            <FaGithub />
+        </div>
+
+    </nav>
+  )
 }
+
+export default NavBar
