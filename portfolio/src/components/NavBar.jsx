6d7a8044f04231dd85a2a103cfeb642ba876fd6d
Kiran9223 <88050617+Kiran9223@users.noreply.github.com>
2024-06-22 14:24:34 +0530

    Project setup

---
+++ portfolio/src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+export default function NavBar() {
+    return(
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>Experience</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
